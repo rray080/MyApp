602b5ec9a71994883f3e0efebc1dc7259d43a3cf
rakesh <rakeshrayk@gmail.com>
2019-05-23 04:23:35 +0530

    Added RadioCheckActivity example
    Signed-off-by: rakesh <rakeshrayk@gmail.com>

---
+++ app/src/main/java/com/rakesh/myapp/CalculatorActivity.kt
@@ -0,0 +1,12 @@
+package com.rakesh.myapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CalculatorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+    }
+}
